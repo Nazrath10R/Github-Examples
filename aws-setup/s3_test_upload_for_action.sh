c6eca169b8325e76aad0415e8d1a988ed3e278eb
Nazrath <nazrath.nawaz@yahoo.de>
2025-03-07 19:05:29 +0000

    semi working eventbridge that triggers a github action

--- aws-setup/s3_test_upload_for_action.sh
+++ aws-setup/s3_test_upload_for_action.sh
@@ -1,4 +1,9 @@
+# aws s3api delete-object --bucket bucket-for-github-action-nn --key example2.csv
 
-# cp ../deprecated/example.csv ../deprecated/example2.csv
-aws s3api delete-object --bucket bucket-for-github-action-nn --key example2.csv
-aws s3 cp ../deprecated/data/example2.csv s3://bucket-for-github-action-nn/data/
+cd /mnt/c/Users/naz/Documents/Github-Examples/
+cp deprecated/data/example.csv deprecated/data/example10.csv
+
+aws s3 cp deprecated/data/example10.csv\
+ s3://bucket-for-github-action-nn/data/
+
+aws logs describe-log-groups --region eu-west-2

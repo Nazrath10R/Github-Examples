87e87a61d73572166c6acd7df982389702247bbb
Nazrath <nazrath.nawaz@yahoo.de>
2025-03-10 13:50:29 +0000

    deleting previous attempt

--- aws-setup/s3_test_upload_for_action.sh
+++
@@ -1,9 +0,0 @@
-# aws s3api delete-object --bucket bucket-for-github-action-nn --key example2.csv
-
-cd /mnt/c/Users/naz/Documents/Github-Examples/
-cp deprecated/data/example.csv deprecated/data/example10.csv
-
-aws s3 cp deprecated/data/example10.csv\
- s3://bucket-for-github-action-nn/data/
-
-aws logs describe-log-groups --region eu-west-2
